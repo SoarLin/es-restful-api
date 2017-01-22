c8bffd3fe74882107bc6a379628ec0be5f6a938c
Soar <soar@stco.tw>
2017-01-22 22:21:19 +0800

    add SSL for production environment

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+'use strict';
+
 var koa = require('koa');
 var route = require('koa-route');
 var app = module.exports = koa();
 var path = require('path');
 var serve = require('koa-static');
+require('dotenv').config({
+    path: '.env'
+});
 
 app.use(serve(path.join(__dirname, '/public')));
 
@@ -21,5 +26,36 @@ var searchRoutes = require('./routes/searchRoutes');
 app.use(route.post('/search/:index/:type', searchRoutes.postSearch));
 
 // Start app up
-app.listen(3000);
-console.log('The app is listening on port 3000.');
+if (process.env.APP_ENV === 'soar') {
+    app.listen(3000);
+    console.log('The app is listening on port 3000.');
+
+} else if (process.env.APP_ENV === 'production') {
+
+    var le = require('letsencrypt-express').create({
+      server: 'https://acme-v01.api.letsencrypt.org/directory'
+    , configDir: '/etc/letsencrypt'
+    , approveDomains: function (opts, certs, cb) {
+        opts.domains = certs && certs.altnames || opts.domains;
+        opts.email = 'soar@stco.tw' // CHANGE ME
+        opts.agreeTos = true;
+
+        cb(null, { options: opts, certs: certs });
+      }
+     , debug: true
+    });
+
+    var https = require('spdy');
+    var server = https.createServer(le.httpsOptions, le.middleware(app.callback()));
+    //console.log(le.httpsOptions);
+    server.listen(3000, function () {
+           console.log('Listening at https://es.quizfun.co:' + this.address().port);
+    });
+    /*
+    var http = require('http');
+    var redirectHttps = koa().use(require('koa-sslify')()).callback();
+    http.createServer(le.middleware(redirectHttps)).listen(80, function () {
+      console.log('handle ACME http-01 challenge and redirect to https');
+    });
+    */
+}
